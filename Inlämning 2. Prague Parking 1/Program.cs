c45d33f9ae476ac5529ec7bc0784d2267c5cf3dd
Robert Vu <vurobert.rv@gmail.com>
2024-10-09 14:04:24 +0200

    Utkast från barinstorm till första arbete.

--- Inlämning 2. Prague Parking 1/Program.cs
+++ Inlämning 2. Prague Parking 1/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Vårt spar plats
+string[] parkingSpaces = new string[100];
+
+// App system
+bool exit = false;
+while (!exit) // True nu
+{
+    Console.WriteLine("Welcome to Prague parking");
+    Console.WriteLine("1. Park Vechicle.");
+    Console.WriteLine("2. Move Vechicle.");
+    Console.WriteLine("3. Get Vechicle.");
+    Console.WriteLine("4. Search Vechicle.");
+    Console.WriteLine("5. Show Vechicle.");
+    Console.WriteLine("6. Exit.");
+
+    string choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            //ParkVehicle();
+            break;
+        case "2":
+            //MoveVehicle();
+            break;
+        case "3":
+            //GetVehicle(); eller RemoveVehicle();
+            break;
+        case "4":
+            //SearchVecicle();
+            break;
+        case "5":
+            //ShowParkingSpaces();
+            break;
+        case "6":
+            exit = true; // ändra till false
+            break;
+        default:
+            Console.WriteLine("Invalid choice, try again.");
+            break;
+    }
+    if (!exit)
+    {
+        Console.WriteLine("Tryck valfri för att fortsätta:");
+        Console.ReadLine();
+    }
+}
+
+
+// Våra metoder vad dom ska utföra.
+
+//ParkVehicle();
+
+//MoveVehicle();
+
+//GetVehicle(); eller RemoveVehicle();
+
+//SearchVecicle();
+
+//ShowParkingSpaces();
